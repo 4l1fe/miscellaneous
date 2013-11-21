d8064ca24d23bc18ce452a04ebc83fc5abae0e67
4l1fe <4l1fe@users.noreply.github.com>
2013-11-21 18:18:28 +0400

    upd

--- kr-generate-autodoc.py
+++ kr-generate-autodoc.py
@@ -1,5 +1,8 @@
 # -*- coding: utf-8 -*-
-import sys, os, shutil, argparse
+import sys
+import os
+import shutil
+import argparse
 import textwrap
 import api_sphinx_examples
 from os.path import join
@@ -13,207 +16,232 @@ import sbis_root
 classes_list, methods_list = list(), list()
 
 
-def prepareSourceDirs(sphinx_root):
-    os.chdir(sphinx_root)
-    if not os.path.exists('.\docs'):
-        os.makedirs('.\docs')
-    if not os.path.exists('.\doc-static') or not os.path.exists('.\doc-static\conf.py') or not os.path.exists('.\doc-static\index.rst'):
-        exit('fatal error: no config or index files')
-    else:
-        try:
-            shutil.rmtree('.\doc-source-genc', ignore_errors=False, onerror=None)
-        except:
-            pass
-        finally:
-            try:
-                shutil.copytree('.\doc-static', '.\doc-source-genc')
-            except:
-                if not os.path.exists('.\doc-source-genc'):
-                    exit('fatal error: doc-source-genc is not found')
-                else:
-                    pass
+def parse_sbis_root():  # Танина необъяснимая логика.
+    sbis_root_dict = sbis_root.__dict__
 
-def getSbisMembers(sphinx_root):
-    sbis_dir = sbis_root.__dict__
-
-    def appendItem(lst, it):
+    def ap_item(lst, it):
         lst.append(it)
 
     switch = {
-                "<class 'Boost.Python.class'>": lambda: appendItem(classes_list, key),
-                "<class 'type'>": lambda: appendItem(classes_list, key),
-                "<class 'Boost.Python.function'>":lambda: appendItem(methods_list, key)
-             }
-    for key in sbis_dir:
-        if (str(type(sbis_dir[key])) != "<class 'type'>"and switch.get(str(type(sbis_dir[key])))) \
-            or (str(type(sbis_dir[key])) == "<class 'type'>" and ord(key[0]) < 128 and key[0].istitle()):
-            switch.get(str(type(sbis_dir[key])))()
-    for _lst in [classes_list, methods_list]:
-        _lst.sort()
+        "<class 'Boost.Python.class'>": lambda: ap_item(classes_list, k),
+        "<class 'type'>": lambda: ap_item(classes_list, k),
+        "<class 'Boost.Python.function'>": lambda: ap_item(methods_list, k)
+    }
 
-def regenSources(sphinx_root):
-    os.chdir(sphinx_root)
+    for k, v in sbis_root_dict.items():
+        string = str(type(v))
+        if (string != "<class 'type'>" and switch.get(string)) \
+            or (string == "<class 'type'>" and ord(k[0]) < 128 and k[0].istitle()):
+            switch.get(string)()
+
+    classes_list.sort()
+    methods_list.sort()
+
+
+def create_rst_files(sphinx_dir):
+    all_classes_str = ""
     req_classes = ['Record', 'RecordSet', 'IField', 'BLObject', 'IStatement', 'IDatabase', 'SqlQuery']
-    list_cl_str = ""
+    sc_name = 'sections/classes'
+    sm_name = 'sections/methods'
+    sect_cls_dir = join(sphinx_dir, sc_name)
+    sect_meth_dir = join(sphinx_dir, sm_name)
+    os.makedirs(sect_cls_dir, exist_ok=True)
+    os.makedirs(sect_meth_dir, exist_ok=True)
 
-    try:
-        os.makedirs('.\doc-source-genc\sections\classes')
-        os.makedirs('.\doc-source-genc\sections\methods')
-    except:
-        if not os.path.exists('.\doc-source-genc\sections'):
-            exit('fatal error: sections folder is not found')
     for cl in classes_list:
-        file = open('.\doc-source-genc\sections\classes\\'+cl+'.rst', mode='w', encoding='utf-8')
+        rst_file_name = join(sect_cls_dir, cl+'.rst')
+        rst_file = open(rst_file_name, mode='w', encoding='utf-8')
         equals = "======"
-        for _char in cl:
+        for _char in cl:  # Знаков = должно быть равное количество длине имени класса.
             equals += "="
 
         # Если текущий класс(cl) входит в req_classes, то к автогенерируемой документации класса и его методов
-        # подставляем кусок кода(code_snippet), описанный в модуле api_sphinx_examples.
-        has_classes = [equal(cl,r_cl) for r_cl in req_classes]
+        # подставляем кусок кода(code_block), описанный в модуле api_sphinx_examples.
+        has_classes = [equal(cl, r_cl) for r_cl in req_classes]
         if not any(has_classes):
-            file.write(".. Класс.\n\nКласс "+cl+"\n"+equals+"\n.. automodule:: sbis_root\n.. autoclass:: "+cl+"\n\t:members:\n\t:private-members:\n\t:special-members:")
+            rst_file.write(
+                ".. Класс.\n\n"
+                "Класс " + cl + "\n"
+                + equals + "\n"
+                ".. automodule:: sbis_root\n"
+                ".. autoclass:: " + cl + "\n"
+                "\t:members:\n"
+                "\t:private-members:\n"
+                "\t:special-members:")
         else:
-            file.write(".. Класс.\n\nКласс "+cl+"\n"+equals+"\n.. automodule:: sbis_root\n.. autoclass:: "+cl+"\n\n\t")
+            rst_file.write(
+                ".. Класс.\n\n"
+                "Класс " + cl + "\n"
+                + equals + "\n"
+                ".. automodule:: sbis_root\n"
+                ".. autoclass:: " + cl + "\n\n\t")
             cl_obj = getattr(sbis_root, cl)
             cl_obj_items = cl_obj.__dict__.items()
             cl_methods = [item for item in cl_obj_items if not item[0].startswith('__')]
 
             for cl_method_name, cl_method_obj in cl_methods:
-                file.write(".. automethod:: "+cl_method_name)
-                file.write("\n\n\t\t")
+                rst_file.write(".. automethod:: " + cl_method_name)
+                rst_file.write("\n\n\t\t")
                 exmp_cl_obj = getattr(api_sphinx_examples, cl, None)
                 exmp_meth_obj = getattr(exmp_cl_obj, cl_method_name, None)
-                code_snippet = getattr(exmp_meth_obj, '__doc__', None)
-                if code_snippet:
-                    code_snippet = code_snippet.splitlines()
-                    code_snippet = '\n'.join([code_line.strip() for code_line in code_snippet])
-                    file.write(".. code-block:: python\n\n")
-                    file.write(textwrap.indent(code_snippet, '\t'*3))
-                    file.write("\n\n\t")
+                code_block = getattr(exmp_meth_obj, '__doc__', None)
+                if code_block:
+                    code_block = code_block.splitlines()
+                    code_block = '\n'.join([code_line.strip() for code_line in code_block])
+                    rst_file.write(".. code-block:: python\n\n")
+                    rst_file.write(textwrap.indent(code_block, '\t' * 3))
+                    rst_file.write("\n\n\t")
                 else:
-                    file.write("\n\n\t")
+                    rst_file.write("\n\n\t")
 
-        list_cl_str += "\t"+"classes/"+cl+"\n"
+        all_classes_str += "\t" + "classes/" + cl + "\n"
 
-    cls_rst = open('.\doc-source-genc\sections\\classes.rst', mode='w', encoding='utf-8')
-    clss_str = ".. СБиС Классы.\n\n"
-    clss_str += "Классы модуля sbis-python\n=========================\n\n"
-    clss_str += ".. toctree::\n\t:maxdepth: 2\n\n" + list_cl_str + "\n"
-    cls_rst.write(clss_str)
-    cls_rst.close()
+    sect_cls_rst = open(join(sphinx_dir, 'sections\classes.rst'), mode='w', encoding='utf-8')
+    template = ".. СБиС Классы.\n\n"
+    template += "Классы модуля sbis-python\n"
+    template += "=========================\n\n"
+    template += ".. toctree::\n"
+    template += "\t:maxdepth: 2\n\n"
+    template += all_classes_str + "\n"
+    sect_cls_rst.write(template)
+    sect_cls_rst.close()
 
-    mth_rst = open('.\doc-source-genc\sections\\methods.rst', mode='w', encoding='utf-8')
-    mth_str = ".. СБиС Методы.\n\n"
-    mth_str += "Методы модуля sbis-python\n============\n\n"
-    mth_str += ".. toctree::\n\t:maxdepth: 2\n\n\tmethods/Rec\n\n"
-    mth_rst.write(mth_str)
-    mth_rst.close()
+    sect_meth_rst = open(join(sphinx_dir, 'sections\methods.rst'), mode='w', encoding='utf-8')
+    template = ".. СБиС Методы.\n\n"
+    template += "Методы модуля sbis-python\n"
+    template += "============\n\n"
+    template += ".. toctree::\n"
+    template += "\t:maxdepth: 2\n\n"
+    template += "\tmethods/Rec\n\n"
+    sect_meth_rst.write(template)
+    sect_meth_rst.close()
 
-    mth_mod = open('.\doc-source-genc\sections\\methods\\Rec.rst', mode='w', encoding='utf-8')
-    equals = "======"
-    mth_mod.write("Методы модуля sbis-python\n=========================\n.. automodule:: sbis_root\n\t:members:\n\t:private-members:\n\t:special-members:")
+    mth_mod = open(join(sect_meth_dir, 'Rec.rst'), mode='w', encoding='utf-8')  #TODO: переименовать
+    mth_mod.write(
+        "Методы модуля sbis-python\n"
+        "=========================\n"
+        ".. automodule:: sbis_root\n"
+        "\t:members:\n"
+        "\t:private-members:\n"
+        "\t:special-members:")
     mth_mod.close()
 
-def rebuildStatic(platform_name, sphinx_root):
-    os.chdir(sphinx_root)
-    os.chdir('.\\' + platform_name + '\www\service')
-    os.system('dir > txt')
-    # os.system('sphinx-build -b html -w sphinx_errors.txt -E -a -c C:\SPHINX_PY_AUTODOC\doc-source-genc C:\SPHINX_PY_AUTODOC\doc-source-genc C:\SPHINX_PY_AUTODOC\docs')
-    os.system('sphinx-build -b html -w sphinx_errors.txt -E -a -c '+join(sphinx_root, 'doc-source-genc ')+join(sphinx_root,'doc-source-genc ')+join(sphinx_root,'docs'))
-    shutil.copy(os.path.abspath('..\..\..\doc-source-genc\index.php'), os.path.abspath('..\..\..\docs'))
+    # Редактирование index.rst. Добавляем ссылки на sc_name, sm_name
+    index_rst = open(join(sphinx_dir, 'index.rst'))
+    template = index_rst.read()
+    index_rst.close()
 
-def copyToDevWI(sphinx_root):
-    def recurseDel(ftpClient, cdir = "/dev-wi.sbis.ru/docs/bl/py", rootdir = "/dev-wi.sbis.ru/docs/bl/py"):
-        ftpClient.cwd(cdir)
-        lister = list()
-        ftpClient.retrlines("NLST", lambda l: lister.append(l))
-        for name in lister:
-            testToDel = cdir + '/' + name
-            try:
-                ftpClient.delete(testToDel)
-            except:
-                try:
-                    ftpClient.rmd(testToDel)
-                except:
-                    recurseDel(ftpClient, testToDel)
-        try:
-            ftpClient.rmd(testToDel)
-        except:
-            try:
-              if testToDel[:testToDel.rfind('/')] != rootdir:
-                ftpClient.rmd(testToDel[:testToDel.rfind('/')])
-                print('in folder:' + testToDel)#.removedirs(testToDel)
-            except:
-                pass
-        return
+    replaced_text = ':maxdepth: 2\n\n'
+    replaced_text += '   {}\n'.format(sm_name)  # в результате не нравится символ - \t
+    replaced_text += '   {}'.format(sc_name)
+    template = template.replace(':maxdepth: 2', replaced_text)
 
-    def recurseUpload(ftpClient, cdir = "/dev-wi.sbis.ru/docs/bl/py", docs = os.path.normpath(sphinx_root + '\docs'), rootdir = "/dev-wi.sbis.ru/docs/bl/py"):
-        try:
-            ftpClient.cwd(cdir)
-        except:
-            pass
-        for name in os.listdir(docs):
-            if name.startswith('.'):
-                _name = name
-                name = name[1:]
-                try:
-                    if not os.path.isfile(os.path.join(docs, name)):
-                        try:
-                            shutil.rmtree(os.path.join(docs, name))
-                        except:
-                            pass
-                    else:
-                        try:
-                            os.remove(os.path.join(docs, name))
-                        except:
-                            pass
-                    os.renames(os.path.join(docs, _name), os.path.join(docs, name))
-                except:
-                    pass
-            if os.path.isfile(os.path.join(docs, name)):
-                f = open(os.path.join(docs, name), "rb")
-                if len(name.split('.')) == 1:
-                    name += '.txt'
-                send = ftpClient.storbinary("STOR "+ name, f)
-            else:
-                try:
-                    ftpClient.mkd(name)
-                except:
-                    print('not create ' + name)
-                recurseUpload(ftpClient, cdir + '/' + name, os.path.join(docs, name))
-                ftpClient.cwd('..')
-        pass
+    index_rst = open(join(sphinx_dir, 'index.rst'), 'w')
+    index_rst.write(template)
+    index_rst.close()
 
-    ftpClient = FTP('dev-wsr-static')
-    ftpClient.login('shuvalovatn', 'TkFFtb7N')
-    ftpClient.cwd("/dev-wi.sbis.ru/docs/bl/py")
-    recurseDel(ftpClient)
-    recurseUpload(ftpClient)
-    ftpClient.close()
+    #Настраиваем conf.py, чтобы подключить автоматический вывод строк документирования
+    conf = open(join(sphinx_dir, 'conf.py'), 'a')
+    service_dir = os.getcwd()
+    template = 'sys.path.insert(0, r"{}")\n'.format(service_dir)
+    template += 'extensions = ["sphinx.ext.autodoc"]'
+    conf.write(template)
+
+
+def sphinx_rebuild(sphinx_dir, prepared_dir):
+    os.system('sphinx-build -b html -w sphinx_errors.txt -E -a ' + sphinx_dir  + ' ' + prepared_dir)
+    #shutil.copy(os.path.abspath('..\..\..\doc-source-genc\index.php'), os.path.abspath('..\..\..\docs'))
+
+
+#def copy_to_dev_wi(sphinx_dir):
+#    def recurseDel(ftpClient, cdir="/dev-wi.sbis.ru/docs/bl/py", rootdir="/dev-wi.sbis.ru/docs/bl/py"):
+#        ftpClient.cwd(cdir)
+#        lister = list()
+#        ftpClient.retrlines("NLST", lambda l: lister.append(l))
+#        for name in lister:
+#            testToDel = cdir + '/' + name
+#            try:
+#                ftpClient.delete(testToDel)
+#            except:
+#                try:
+#                    ftpClient.rmd(testToDel)
+#                except:
+#                    recurseDel(ftpClient, testToDel)
+#        try:
+#            ftpClient.rmd(testToDel)
+#        except:
+#            try:
+#                if testToDel[:testToDel.rfind('/')] != rootdir:
+#                    ftpClient.rmd(testToDel[:testToDel.rfind('/')])
+#                    print('in folder:' + testToDel)#.removedirs(testToDel)
+#            except:
+#                pass
+#        return
+#
+#    def recurseUpload(ftpClient, cdir="/dev-wi.sbis.ru/docs/bl/py", docs=os.path.normpath(sphinx_dir + '\docs'),
+#                      rootdir="/dev-wi.sbis.ru/docs/bl/py"):
+#        try:
+#            ftpClient.cwd(cdir)
+#        except:
+#            pass
+#        for name in os.listdir(docs):
+#            if name.startswith('.'):
+#                _name = name
+#                name = name[1:]
+#                try:
+#                    if not os.path.isfile(os.path.join(docs, name)):
+#                        try:
+#                            shutil.rmtree(os.path.join(docs, name))
+#                        except:
+#                            pass
+#                    else:
+#                        try:
+#                            os.remove(os.path.join(docs, name))
+#                        except:
+#                            pass
+#                    os.renames(os.path.join(docs, _name), os.path.join(docs, name))
+#                except:
+#                    pass
+#            if os.path.isfile(os.path.join(docs, name)):
+#                f = open(os.path.join(docs, name), "rb")
+#                if len(name.split('.')) == 1:
+#                    name += '.txt'
+#                send = ftpClient.storbinary("STOR " + name, f)
+#            else:
+#                try:
+#                    ftpClient.mkd(name)
+#                except:
+#                    print('not create ' + name)
+#                recurseUpload(ftpClient, cdir + '/' + name, os.path.join(docs, name))
+#                ftpClient.cwd('..')
+#        pass
+#
+#    ftpClient = FTP('dev-wsr-static')
+#    ftpClient.login('shuvalovatn', 'TkFFtb7N')
+#    ftpClient.cwd("/dev-wi.sbis.ru/docs/bl/py")
+#    recurseDel(ftpClient)
+#    recurseUpload(ftpClient)
+#    ftpClient.close()
+
 
 def main():
-    #парсим аргументы из командной строки
     parser = argparse.ArgumentParser()
-    parser.add_argument('-pn', '--platform_name', action='store', type=str, dest='platform_name', required=True)
-    parser.add_argument('-sr', '--sphinx_root', action='store', type=str, dest='sphinx_root', required=True)
-    res_args = parser.parse_args()
-    platform_name = res_args.platform_name
-    sphinx_root = res_args.sphinx_root
-
-    #Проверяем, что папка с начальными ресурсами существует. Готовим директорию для хранения исходных ресурсов автодокументации.
-    prepareSourceDirs(sphinx_root)
+    parser.add_argument('-sd', '--sphinx_dir', action='store', type=str, dest='sphinx_dir', required=True)
+    result = parser.parse_args()
 
     #Получаем список классов Python API для генерации ресурсов сайта.
-    getSbisMembers(sphinx_root)
-    # if isSuccessUpdate:
+    parse_sbis_root()
+
     #Перегенерируем rst файлы с обновленными классами
-    regenSources(sphinx_root)
-    #Делаем ребилд документации с обновленными ресурсами
-    rebuildStatic(platform_name, sphinx_root)
+    create_rst_files(result.sphinx_dir)
+
+    #Делаем ребилд документации с обновленными rst файлами.
+    prepared_dir = join(result.sphinx_dir, 'prepared_dir')
+    sphinx_rebuild(result.sphinx_dir, prepared_dir)
 
     #Копируем по FTP на dev-wi.sbis.ru
-    copyToDevWI(sphinx_root)
+    #copy_to_dev_wi(result.sphinx_dir)
+
 
 if __name__ == '__main__':
     main()
