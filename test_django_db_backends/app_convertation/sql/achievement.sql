3f7cc30e30904b20592df86a5cb514e5e6188e34
4l1fe <4l1fe@users.noreply.github.com>
2014-02-07 13:37:21 +0400

    добавил тесты движков БД на джанге

---
+++ test_django_db_backends/app_convertation/sql/achievement.sql
@@ -0,0 +1,5 @@
+INSERT INTO `achievement` (`name`, `cost`, `login_id`) VALUES ('winner', 0, 2);
+INSERT INTO `achievement` (`name`, `cost`, `login_id`) VALUES ('pro', 0, 2);
+INSERT INTO `achievement` (`name`, `cost`, `login_id`) VALUES ('pro', 0, 1);
+INSERT INTO `achievement` (`name`, `cost`, `login_id`) VALUES ('eng', 0, 1);
+INSERT INTO `achievement` (`name`, `cost`, `login_id`) VALUES ('eng', 0, 2);
